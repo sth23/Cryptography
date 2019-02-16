942c19e0409a2207eaa78bfc7a678754bd18f879
sth23 <47009471+sth23@users.noreply.github.com>
2019-02-15 19:30:43 -0500

    Updated from Brython Server: 2/15/2019 7:30:44 PM

--- cryptography.py
+++ cryptography.py
@@ -1,7 +1,7 @@
 """
 cryptography.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Sean Healey
+Credit: Tutorials
 
 Assignment:
 
@@ -10,3 +10,25 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+
+# Encrypt function, x = unecrypted string, y = key
+def encrypt(x,y):
+    return x
+
+# Decrypt function, x = encrypted string, y = key
+def decrypt(x,y):
+    return x
+
+process = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+if process == "e":
+    message = input("Message: ")
+    key = input("Key: ")
+    print(encrypt(message, key))
+elif process == "d":
+    message = input("Message: ")
+    key = input("Key: ")
+    print(decrypt(message, key)
+elif process == "q":
+    Goodbye!
+else:
+    print("Did not understand command, try again.")
