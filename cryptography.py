419ff23213b6490fa5a197ab385bf48220246e39
sth23 <47009471+sth23@users.noreply.github.com>
2019-02-15 19:59:44 -0500

    Updated from Brython Server: 2/15/2019 7:59:45 PM

--- cryptography.py
+++ cryptography.py
@@ -1,7 +1,7 @@
 """
 cryptography.py
 Author: Sean Healey
-Credit: Tutorials
+Credit: Tutorials, Stack Overflow
 
 Assignment:
 
@@ -13,22 +13,27 @@ associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .
 
 # Encrypt function, x = unecrypted string, y = key
 def encrypt(x,y):
+    message_chars = list(x)
+    key_chars = list(y)
     return x
 
 # Decrypt function, x = encrypted string, y = key
 def decrypt(x,y):
     return x
 
-process = input("Enter e to encrypt, d to decrypt, or q to quit: ")
-if process == "e":
-    message = input("Message: ")
-    key = input("Key: ")
-    print(encrypt(message, key))
-elif process == "d":
-    message = input("Message: ")
-    key = input("Key: ")
-    print(decrypt(message, key))
-elif process == "q":
-    print("Goodbye!")
-else:
-    print("Did not understand command, try again.")
+# Takes user input to initiate program
+process = ""
+while process != "q": # program runs as long as process != q
+    process = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    if process == "e":
+        message = input("Message: ")
+        key = input("Key: ")
+        print(encrypt(message, key))
+    elif process == "d":
+        message = input("Message: ")
+        key = input("Key: ")
+        print(decrypt(message, key))
+    elif process == "q":
+        print("Goodbye!")
+    else:
+        print("Did not understand command, try again.")
